6e5bedeb2d3f0f8c57c2fe8e34c3caa56c6423b6
akira9981 <56640109+akira9981@users.noreply.github.com>
2020-05-07 03:01:39 +0900

    サムネイルダミー画像

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,11 @@
     <div class="col-md-4">
         <div class="card mb50">
             <div class="card-body">
+            @if(!empty($review->image))
+                <div class='image-wrapper'><img class='book-image' src="{{ asset('storage/images/'.$review->image) }}"></div>
+            @else
                 <div class='image-wrapper'><img class='book-image' src="{{ asset('images/guitar.png') }}"></div>
+            @endif
                 <h3 class='h3 book-title'>{{ $review->title }}</h3>
                 <p class='description'>
                     {{ $review->body }}
