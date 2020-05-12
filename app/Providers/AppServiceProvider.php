f67c33463cd4964cd8b8344a6e4e9311bb96d8ce
akira9981 <56640109+akira9981@users.noreply.github.com>
2020-05-13 01:40:00 +0900

    https 変更

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        if(\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
-}
