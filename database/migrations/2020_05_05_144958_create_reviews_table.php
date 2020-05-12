141fa299837e48147a967b01b7352f16263b6330
akira9981 <56640109+akira9981@users.noreply.github.com>
2020-05-13 01:22:36 +0900

    delete update timestamp

--- database/migrations/2020_05_05_144958_create_reviews_table.php
+++ database/migrations/2020_05_05_144958_create_reviews_table.php
@@ -21,7 +21,6 @@ class CreateReviewsTable extends Migration
             $table->longText('body');
             $table->string('image')->nullable();
             $table->tinyInteger('status')->default(1)->comment('0=下書き, 1=アクティブ, 2=削除済み');
-            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
         });
     }
